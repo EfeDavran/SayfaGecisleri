c7d1d0768ec0cacd33eea42bae55983d4d2c6712
Efe Davran <115793512+EfeDavran@users.noreply.github.com>
2024-09-25 16:23:13 +0300

    SayfaGecisleri
    Innova Bilisim & BTK Akademi iOS Bootcamp Atolyesi Odev

---
+++ SayfaGecisleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SayfaGecisleri
+//
+//  Created by EFE DAVRAN on 25.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
